3931d7cab0f036872baafdbff7f12ec6870c3db2
AKAS-H102 <aravindhakash805@gmail.com>
2024-08-20 12:15:01 +0530

    c program

---
+++ basic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value A");
+    scanf("%d",&a);
+    printf("Enter the value B");
+    scanf("%d",&b);
+    if(a>b){
+        printf("A is greater than B");
+    }else{
+        printf("B is greater than A");
+    }
+}
